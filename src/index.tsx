d119cea5b75a0c4e730c78ab2a7fb98652652e44
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-07-19 00:07:39 +0900

    [Add] tailwind config

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
+import "./styles/styles.css"
 
 ReactDOM.render(
   <React.StrictMode>
