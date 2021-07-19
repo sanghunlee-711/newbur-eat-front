19b09321ec721665f5d19e0f7b7ae771c1419c60
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-07-19 21:49:40 +0900

    [Add] handling react-hook-form - 기본적으로 error handle, state 확인 , validation 기능을 모두   제공해준다 - require 메서드를 이용해서 해당 기능들을 사용하면   됨.(loggedoutrouter참고)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useReactiveVar } from '@apollo/client';
 import React from 'react';
 import { isLoggedInVar } from './apollo';
-import { LoggedInRouter } from './routers/logged-in-router';
+import LoggedInRouter from './routers/logged-in-router';
 import { LoggedOutRouter } from './routers/logged-out-router';
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
-  console.log(isLoggedIn);
   return <>{isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />}</>;
 }
 
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,15 +1,64 @@
-import { isLoggedInVar } from '../apollo';
+import { useForm } from 'react-hook-form';
+
+interface IForm {
+  email: string;
+  password: string;
+}
 
 export const LoggedOutRouter = () => {
-  const onClick = () => {
-    isLoggedInVar(true);
-    console.log('..');
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>();
+
+  const onSubmit = () => {
+    console.log(watch());
+  };
+  const onInvalid = () => {
+    console.log('cant create account');
+    console.log(errors);
   };
 
   return (
     <div>
       <h1>Logged out</h1>
-      <button onClick={onClick}>Click to login</button>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            {...register('email', {
+              required: true,
+              pattern: /^[A-Za-z0-9._$+=]+gmail.com$/,
+            })}
+            type="email"
+            name="email"
+            required
+            placeholder="email"
+          />
+          {errors.email?.message && (
+            <span className="bg-yellow-200">{errors.email.message}</span>
+          )}
+
+          {errors.email?.type === 'pattern' && (
+            <span className="red">Only gmail allowed</span>
+          )}
+        </div>
+        <div>
+          <input
+            {...register('password', {
+              required: true,
+            })}
+            type="password"
+            name="password"
+            required
+            placeholder="password"
+          />
+        </div>
+        <button type="submit" className="bg-yellow-100 text-white">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
