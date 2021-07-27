13f07c0c49cf58aa50c70b5ffa13098a0720e831
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-07-27 22:08:40 +0900

    [Add] 테스트케이스추가 > app.tsx - jest mock을 통해서 컴포넌트 모킹방법 - waitFor를 통해서 state업데이트 하는 방법 - render메서드를 통해 받는 함수로 여러종류가 있는데 getByText를 통해   렌더여부 확인 가능

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { client } from './apollo';
-import App from './App';
+import { App } from './components/app';
 import reportWebVitals from './reportWebVitals';
 import './styles/styles.css';
 
