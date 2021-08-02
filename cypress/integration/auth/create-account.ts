81fe08124d4eafea64d46813791170303bd353b1
cloudlee <cloudlee@MacBook-Pro-9.local>
2021-08-03 00:20:01 +0900

    [Add] E2E test case > create-account - 신기한 cypress의 세계 - 대부분 기능을 지원해주는 것 같고 @testing-library/cypress가 있어야   dom을 캐치하기 더 쉬운것 같다

---
+++ cypress/integration/auth/create-account.ts
@@ -0,0 +1,32 @@
+describe('Create Account', () => {
+  const user = cy;
+  it('Should see email / password validation errors', () => {
+    user.visit('/');
+    user.findByText(/Create an Account/i).click();
+    user.findAllByPlaceholderText(/email/i).type('bad@email');
+    user.findByRole('alert').should('have.text', 'Please Enter a valid email');
+    user.findAllByPlaceholderText(/email/i).clear();
+    user.findByRole('alert').should('have.text', 'Email is required');
+
+    user.findAllByPlaceholderText(/email/i).type('good@email.com');
+    user.findAllByPlaceholderText(/password/i).type('bad');
+    user.findAllByPlaceholderText(/password/i).clear();
+    user.findByRole('alert').should('have.text', 'Password is required');
+  });
+
+  it('Shoulde be able to create account and login ', () => {
+    user.visit('/create-account');
+
+    user.findAllByPlaceholderText(/email/i).type('real@test.com');
+    user.findAllByPlaceholderText(/password/i).type('test');
+    user.findByRole('button').click();
+
+    user.wait(1000);
+
+    user.findByPlaceholderText(/email/i).type('real@test.com');
+    user.findAllByPlaceholderText(/password/i).type('test');
+    user.findByRole('button').click();
+
+    user.window().its('localStorage.newber-token').should('be.a', 'string');
+  });
+});
